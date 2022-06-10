8b7a198fa705f126c76edefb3a46b07677305571
tonyprashanth1996 <107090682+tonyprashanth1996@users.noreply.github.com>
2022-06-10 15:30:31 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World 12th june!");
+    System.out.println("Hello World 11th june!");
     }
 }
